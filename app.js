6df83ae234e67c3b79f9ddd0cdd239ff5c6a2358
molto <bodymgahed@yahoo.com>
2022-08-11 07:30:00 +0200

    js is finshed.

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const container = document.querySelector("#container");
-
 const content = document.createElement("div");
+const p = document.createElement("p");
+const h3 = document.createElement("h3");
+const div = document.createElement("div");
+const divH1 = document.createElement("h1");
+const divP = document.createElement("p");
+
 content.classList.add("content");
+p.style = "color : red";
+h3.style = "color : blue";
+div.style = "border:2px solid black; background-color:pink;";
+
 content.textContent = "This is the glorious text-content!";
+p.textContent = "hey i'am red!";
+h3.textContent = "I'am a blue h3!";
+divH1.textContent = "I’m in a div";
+divP.textContent = "me too";
 
 container.appendChild(content);
+container.appendChild(p);
+container.appendChild(h3);
+container.appendChild(div);
+container.appendChild(div);
+div.appendChild(divH1);
+div.appendChild(divP);
